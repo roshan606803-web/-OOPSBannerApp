bbaac1edddfe83618df11bf9b6f93ebbab7b46af
Roshan.A <roshan606803@gmail.com>
2026-03-03 09:36:04 +0530

    UC3: Refactored banner using String.join() for memory efficiency

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,21 +1,62 @@
 /**
- * OOPSBannerApp - Use Case 2
- * Prints the word "OOPS" in large banner format.
+ * OOPSBannerApp - Use Case 3
+ * Prints the word "OOPS" using String.join() for better memory efficiency.
  *
  * @author Roshan
- * @version 2.0
+ * @version 3.0
  */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println(" *****     *****    *****    *****");
-        System.out.println("*     *   *     *  *     *  *     ");
-        System.out.println("*     *   *     *  *     *  *     ");
-        System.out.println("*     *   *****    *****     ***** ");
-        System.out.println("*     *   *        *             * ");
-        System.out.println("*     *   *        *             * ");
-        System.out.println(" *****    *        *        *****  ");
+        String line1 = String.join("   ",
+                " ***** ",
+                " ***** ",
+                " ******",
+                " ******");
 
+        String line2 = String.join("   ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*      ");
+
+        String line3 = String.join("   ",
+                "*     *",
+                "*     *",
+                "****** ",
+                " ***** ");
+
+        String line4 = String.join("   ",
+                "*     *",
+                "*     *",
+                "*      ",
+                "      *");
+
+        String line5 = String.join("   ",
+                "*     *",
+                "*     *",
+                "*      ",
+                "*     *");
+
+        String line6 = String.join("   ",
+                "*     *",
+                "*     *",
+                "*      ",
+                "*     *");
+
+        String line7 = String.join("   ",
+                " ***** ",
+                " ***** ",
+                "*      ",
+                " ***** ");
+
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+        System.out.println(line4);
+        System.out.println(line5);
+        System.out.println(line6);
+        System.out.println(line7);
     }
 }
